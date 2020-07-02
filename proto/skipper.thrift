0f385041c28743fc3053d93de5e167db251ce05a
d.baykov <d.baykov@rbkmoney.com>
2020-07-02 16:40:24 +0300

    BJ-943: Protocol implementataion. Fix after review

--- proto/skipper.thrift
+++ proto/skipper.thrift
@@ -5,11 +5,9 @@ include "base.thrift"
 
 typedef base.ID ChargebackID
 
-typedef base.ID CrmTicketID
-
 struct ChargebackGeneralData {
     1: required base.Timestamp     pretension_date
-    2: required base.ID            acquirer_id
+    2: required base.ID            provider_id
     3: required base.Timestamp     operation_date
     4: required base.ID            invoice_id
     5: required base.ID            payment_id
@@ -24,74 +22,76 @@ struct ChargebackGeneralData {
     14: optional ChargebackReason  chargeback_reason
 }
 
-struct ChargebackData {
-    1: required ChargebackGeneralData  general_data
-    2: required list<StatusChange>     status_shanges
-    3: optional list<Comment>          comments
-    4: required HoldStatus             hold_status
-}
-
 struct ChargebackReason {
     1: optional base.ID            code
     2: required ChargebackCategory category
 }
 
-enum ChargebackCategory {
-    fraud
-    dispute
-    authorisation
-    processing_error
+union ChargebackCategory {
+    1: ChargebackCategoryFraud           fraud
+    2: ChargebackCategoryDispute         dispute
+    3: ChargebackCategoryAuthorisation   authorisation
+    4: ChargebackCategoryProcessingError processing_error
 }
 
-struct HoldStatus {
-    1: optional bool                will_hold_from_merchant
-    2: optional bool                was_hold_from_merchant
-    3: optional bool                hold_from_us
+struct ChargebackCategoryFraud           {}
+struct ChargebackCategoryDispute         {}
+struct ChargebackCategoryAuthorisation   {}
+struct ChargebackCategoryProcessingError {}
+
+union ChargebackStage {
+    1: StageChargeback     chargeback
+    2: StagePreArbitration pre_arbitration
+    3: StageArbitration    arbitration
 }
 
-struct StatusChange {
-    1: required ChargebackStep      step
-    2: required ChargebackStatus    status
-    3: optional base.Timestamp      created_at
-    4: optional Comment             comment
-    5: optional base.Timestamp      date_of_decision
+struct StageChargeback     {}
+struct StagePreArbitration {}
+struct StageArbitration    {}
+
+union ChargebackStatus {
+    1: ChargebackPending   pending
+    2: ChargebackAccepted  accepted
+    3: ChargebackRejected  rejected
+    4: ChargebackCancelled cancelled
 }
 
-enum ChargebackStep {
-    chargeback
-    pre_arbitration
-    arbitration
+struct ChargebackPending   {}
+struct ChargebackAccepted  {}
+struct ChargebackRejected  {}
+struct ChargebackCancelled {}
+
+
+union ChargebackEvent {
+    1: ChargebackCreateEvent           create_event
+    2: ChargebackStatusChangeEvent     status_change_event
+    3: ChargebackHoldStatusChangeEvent hold_status_change_event
 }
 
-enum ChargebackStatus {
-    pending
-    accepted
-    rejected
-    cancelled
+struct ChargebackCreateEvent {
+    1: required ChargebackGeneralData creation_data
 }
 
-enum CommentOwnerType {
-    merchant
-    bank
-    processing
+struct ChargebackStatusChangeEvent {
+    1: required base.ID            invoice_id
+    2: required base.ID            payment_id
+    3: required ChargebackStage     stage
+    4: required ChargebackStatus    status
+    5: optional base.Timestamp      created_at
+    6: optional base.Timestamp      date_of_decision
 }
 
-struct CrmTicket {
-    1: required CrmTicketID ticket_id
-    2: optional CrmTicketInfo ticket_info
+struct ChargebackHoldStatusChangeEvent {
+    1: required base.ID            invoice_id
+    2: required base.ID            payment_id
+    3: optional bool                will_hold_from_merchant
+    4: optional bool                was_hold_from_merchant
+    5: optional bool                hold_from_us
 }
 
-struct CrmTicketInfo {
-    1: string comment
-    2: string label
-    3: string ticket_state
-}
-
-struct Comment {
-    1: required string              message
-    2: optional base.Timestamp      created_at
-    3: required CommentOwnerType    comment_owner_type
-    4: required string              comment_owner_name
+struct ChargebackData {
+    1: required ChargebackID           id
+    2: required list<ChargebackEvent>  events
 }
 
 exception ChargebackCreationException {}
@@ -99,27 +99,15 @@ exception ChargebackCreationException {}
 /** Service for work with chargebacks */
 service Skipper {
 
-    ChargebackID RetrievalRequest(1: ChargebackGeneralData creation_data) throws (1: ChargebackCreationException ex1)
+    void processChargebackData(1: ChargebackEvent event)
 
-    ChargebackID CreateChargeback(1: ChargebackGeneralData creation_data) throws (1: ChargebackCreationException ex1)
+    ChargebackData getChargebackData(base.ID invoice_id, base.ID payment_id)
 
-    void ChangeStatus(base.ID invoice_id, base.ID payment_id, StatusChange status_change)
-
-    void SetHoldStatus(base.ID invoice_id, base.ID payment_id, HoldStatus hold_status)
-
-    void AddComment(base.ID invoice_id, base.ID payment_id, Comment comment)
-
-    ChargebackData getChargeback(base.ID invoice_id, base.ID payment_id)
-
-    list<ChargebackData> getChargebacksByStep(ChargebackStep step, ChargebackStatus status)
+    list<ChargebackData> getChargebacksByStep(ChargebackStage step, ChargebackStatus status)
 
     list<ChargebackData> getChargebacksByDate(base.Timestamp date_from, base.Timestamp date_to)
 
-    list<ChargebackData> getChargebacksByAcquirerId(string acquirer_id, list<ChargebackStatus> statuses)
-
-    CrmTicketID CreateCrmTicket(ChargebackID chargeback_id, CrmTicketInfo ticket_info)
-
-    CrmTicket GetCrmTicketById(ChargebackID chargeback_id)
+    list<ChargebackData> getChargebacksByProviderId(string provider_id, list<ChargebackStatus> statuses)
 
 }
 
